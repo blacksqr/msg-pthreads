648bfd0a99ddaf3406342f57648909cd91a3d22f
goldale@web.de <goldale@web.de@c3091dde-1444-0410-a9eb-b99409f43359>
2008-01-19 18:21:00 +0000

    Add License to all source files

--- trunk/src/tclAdapter.cpp
+++ trunk/src/tclAdapter.cpp
@@ -1,4 +1,15 @@
-// $Id$
+/* The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ * The Initial Developer of the Original Code is Alex Goldenstein.
+ * All Rights Reserved.
+ * Contributor(s): Alex Goldenstein.<goldale.de@googlemail.com>
+ */
 #include <dprint.h>
 #include <tclAdapter.h>
 
