ab1fd650ce253832403c552da92ca65d5f1f659d
goldale@web.de <goldale@web.de@c3091dde-1444-0410-a9eb-b99409f43359>
2008-01-30 20:23:00 +0000

    New set env script

--- trunk/set_env.sh
+++ trunk/set_env.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../Tcl/unix:../Sql/.libs:../BDb/.libs
+ITS_APP_HOME=`pwd`
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ITS_APP_HOME/Tcl/unix:$ITS_APP_HOME/Sql/.libs:$ITS_APP_HOME/BDb/.libs
+export ITS_APP_HOME=$ITS_APP_HOME/src
